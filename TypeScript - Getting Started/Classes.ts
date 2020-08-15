e26569837a03366aa3645d19993790ad1c405316
Георги <gtk960311@gmail.com>
2020-08-15 17:16:05 +0300

    Getting used with modules

--- TypeScript - Getting Started/Classes.ts
+++ TypeScript - Getting Started/Classes.ts
@@ -1,4 +1,4 @@
-class Custoemr {
+export class Customer {
   name: string;
   isActive: boolean;
 
@@ -7,10 +7,19 @@ class Custoemr {
     this.isActive = true;
   }
 
+  //access modifiers:
+  //default public - all is accessible outside of the code
+  //private - can call the method only from inside the class
+  //protected - can call from inside the class + from classes which inherits the this one but no outside
+
   announce() {
     return "Hello, customer " + this.name;
   }
 }
 
 // create a new instance
-let firstCustomer = new Custoemr("Alice");
+let firstCustomer = new Customer("Alice");
+
+class SpecialCustomer extends Customer {
+  // inherits data and behaviour of Customer and
+}
---
+++ TypeScript - Getting Started/ModulesExample.ts
@@ -0,0 +1,4 @@
+import { Customer } from "./Classes";
+
+let myCustomer = new Customer("Bob");
+console.log(myCustomer.announce());
