49b6f725c80c2222c3897c588117fcd50a19cf0e
Георги <gtk960311@gmail.com>
2020-08-16 23:02:41 +0300

    Finishing configuration.

--- TypeScript - Getting Started/webpack.config.js
+++ TypeScript - Getting Started/webpack.config.js
@@ -1,8 +1,13 @@
 module.exports = {
   entry: './app/app.ts',
   devtool: 'inline-source-map',
+  module: {
+    rules: [
+      { test: /\.ts$/, exclude: /node_modules/, loader: 'ts-loader' }
+    ]
+  },
   resolve: {
-    extensions: [ '.tsx', '.ts', '.js' ]
+    extensions: ['.tsx', '.ts', '.js']
   },
   output: {
     filename: 'bundle.js'
